5191ca0848acf0ca2d8520d7d534af4024636023
PerryNusbaum <151457844+PerryNusbaum@users.noreply.github.com>
2024-03-19 12:55:11 +0200

    Delete src/store/SimpleBank .sol

--- src/store/SimpleBank .sol
+++
@@ -1,9 +0,0 @@
-//declarration the file compiler
-pragma solidity ^0.6.6;
-
-//three slashes used to write documentation,
-// descriptive data for UI elements.
-
-/// @title SimpleBank
-/// @author Perry Nusbaum
-
