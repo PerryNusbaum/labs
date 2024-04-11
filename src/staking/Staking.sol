1105b79fdb19a2ee338926955669f629cad1fccc
perryNusbaum <pn0534129@gmail.com>
2024-04-11 16:43:39 +0300

    Apr11

--- src/staking/Staking.sol
+++ src/staking/Staking.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.24;
 
+import {console} from "forge-std/Test.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../myCoin/MyERC20.sol";
 
-contract Staking is ERC20{
+contract Staking {
 
-    IERC20 public immutable stakingToken;
+    MyERC20 public immutable stakingToken;
 
     address public owner;
 
@@ -17,15 +17,15 @@ contract Staking is ERC20{
     
     uint256 public rewardAmount = 1000000; 
     
-    constructor () ERC20 ("myToken","MTK"){
+    constructor (address token) {
         owner = msg.sender;
-        stakingToken = IERC20(address(this));
-        _mint (address(this), rewardAmount);
+        stakingToken = MyERC20(token);
+        stakingToken.mint (address(this), rewardAmount);
     }
 
     function deposit(uint amount) external{
         require(amount > 0, "amount = 0");
-        stakingToken.transfer(address(this), amount);
+        stakingToken.transferFrom(msg.sender,address(this), amount);
         usersDeposit[msg.sender][block.timestamp] = amount;
         numOfDeposit[msg.sender] += 1;
     } 
@@ -50,8 +50,10 @@ contract Staking is ERC20{
             //variable time = the time he deposit
             uint time = usersDeposit[msg.sender][i];
             // Check if the timestamp is valid (not 0) and if it's been more than a week
+            console.log(time);
             require(block.timestamp > time + 1 weeks, "Time has not passed");
             uint depositedAmount = usersDeposit[msg.sender][time];
+            require(depositedAmount > 0, "vjhbjk");
             if (amount <= depositedAmount) {
                 usersDeposit[msg.sender][time] -= amount;
                 amount=0;
@@ -63,7 +65,7 @@ contract Staking is ERC20{
         require(amount == 0, "you cant withdraw this amount");        
         // Calculate and transfer rewards
         uint rewardToSend = calculateRewards(amount);
-        stakingToken.transferFrom(address(this), msg.sender, rewardToSend + amount * 10 ** uint(decimals()));
+        stakingToken.transferFrom(address(this), msg.sender, rewardToSend + amount * 10 ** 18);
         rewardAmount -= rewardToSend;
     }
 
--- test/staking/Staking.t.sol
+++ test/staking/Staking.t.sol
@@ -4,20 +4,43 @@ pragma solidity ^0.8.24;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import "@hack/staking/Staking.sol";
+import "@hack/myCoin/MyERC20.sol";
 
 contract StakingTest is Test{
     Staking public s;
+    MyERC20 public token;
 
     function setUp() public{
-        s = new Staking();
+        token = new MyERC20();
+        s = new Staking(address(token));
     } 
 
     function testDeposit() public{
-        uint initialBalance = address(s).balance;
+        uint initialBalance = s.getBalance();
         console.log(address(s).balance);
+        console.log(address(s));
+        console.log(address(this).balance);
         uint amount = 120;
+        token.mint(address(this),amount);
+        token.approve(address(s),amount);
         s.deposit(amount);
-        console.log(address(s).balance);
-        assertEq(address(s).balance - initialBalance, amount);
+        console.log(s.getBalance());
+        console.log(initialBalance);
+        assertEq(s.getBalance() - initialBalance, amount);
+        // s.usersDeposit
+    }
+
+    function testWithdraw() public{
+        uint amount = 120;
+        token.mint(address(this),amount);
+        token.approve(address(s),amount);
+        s.deposit(amount);
+        vm.warp(block.timestamp + 7 days);
+        uint initialBalance = s.getBalance();
+        console.log(initialBalance);
+        s.withdraw(amount);
+        uint afterBalance = s.getBalance();
+        console.log(afterBalance);
+        assertEq(initialBalance - amount, afterBalance);
     }
 }
