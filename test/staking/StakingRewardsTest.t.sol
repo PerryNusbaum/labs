acd6827abbdb267ea8e8fa8bdf409cd1f3adc8b4
perryNusbaum <pn0534129@gmail.com>
2024-05-02 15:56:53 +0300

    May2

--- test/staking/StakingRewardsTest.t.sol
+++ test/staking/StakingRewardsTest.t.sol
@@ -9,7 +9,7 @@ import "@hack/myCoin/MyERC20.sol";
 
 contract StakingRewardsTest is Test{
     uint wad = 10 ** 18;
-    StakingRewards public nms;
+    StakingRewards public sr;
     // MyERC20 st;
     // MyERC20 rt;
     MyERC20 public st;
@@ -23,14 +23,65 @@ contract StakingRewardsTest is Test{
         st = new MyERC20("StekingToken","ST");
         rt = new MyERC20("RewardToken","RT");
 
-        nms = new StakingRewards(address(st), address(rt));
+        sr = new StakingRewards(address(st), address(rt));
 
-        rt.mint(address(nms), 100000 * wad);
+        rt.mint(address(sr), 100000 * wad);
 
         st.mint(user1, 100 * wad);
         st.mint(user2, 100 * wad);
         st.mint(user3, 100 * wad);
     }
+
+    // function stakeTest() public{
+    //     vm.startPrank(user1);
+    //     sr.stake(100 * wad);
+    //     vm.warp(block.timestamp + 2 days);
+    //     sr.getReward();
+    //     assertEq(user1.balance, 172800 * 0.0016);
+    //     vm.stopPrank();
+    // }
+// function stakeTest() public {
+//     // Store initial balance of user1
+//     uint256 initialBalance = user1.balance;
+    
+//     // Start the staking process
+//     vm.startPrank(user1);
+//     sr.stake(100 * wad);
+    
+//     // Fast forward time by 2 days
+//     vm.warp(block.timestamp + 2 days);
+    
+//     // Claim reward
+//     sr.getReward();
+    
+//     // Calculate reward amount
+//     uint256 rewardAmount = (172800 * 16) / 10000; // Calculate reward amount as a separate step
+    
+//     // Calculate expected balance after staking and reward claim
+//     uint256 expectedBalance = initialBalance + rewardAmount;
+    
+//     // Assert the balance of user1
+//     assertEq(user1.balance, expectedBalance);
+    
+//     // Stop the staking process
+//     vm.stopPrank();
+// }
+
+function testStake()public {
+        vm.startPrank(user1);
+        uint256 initialBalance = user1.balance;
+        st.approve(address(sr),100);
+        sr.stake(100);
+        vm.warp(block.timestamp + 2 days);
+        sr.getReward();
+        uint256 balance = address(user1).balance;
+        uint256 scale = 10000;
+        uint256 scaledMultiplier = 16;
+        uint256 twoDays = (172800 * scaledMultiplier) / scale;
+        assertEq(balance, initialBalance + twoDays, "wrong!!!" );
+        vm.stopPrank();
+    }
+
 
 }
 
