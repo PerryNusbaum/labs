87d8e6c2dbfd3d8bb8bbfe2c76dbf0d739c52c46
root <root@DESKTOP-N26DVKE>
2024-05-08 16:39:51 +0300

    May8

---
+++ test/AMM/CP.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "@hack/amm/CP.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@hack/myCoin/MyERC20.sol";
+
+
+contract CPTest is Test{
+    CP public cp;
+    MyERC20 public t1;
+    MyERC20 public t2;
+
+    function setUp() public{
+        t1 = new MyERC20("token1", "t1");
+        t2 = new MyERC20("token2", "t2");
+        cp = new CP(address (t1), address (t2));
+    }
+    
+    function testSwap() public{
+        address user = vm.addr(1);
+        vm.startPrank(user);
+        t1.mint(address(user),200);
+        t2.mint(address(user),200);
+        t1.approve(address(cp),50);
+        t2.approve(address(cp),90);
+        cp.addLiquidity(50, 90);
+        console.log(address(t1));
+        uint initial2 = t2.balanceOf(address(cp));
+        uint initial1 = t1.balanceOf(address(cp));
+        console.log(t1.balanceOf(address(cp)));
+        t1.approve(address(cp),20);
+
+        cp.swap(address(t1), 20);
+        console.log(t1.balanceOf(address(cp)));
+        console.log(t2.balanceOf(address(cp)));
+    }
+}
