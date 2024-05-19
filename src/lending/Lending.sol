92f05e0548c526a88b2e1d5bda3e3745f9f2b761
perryNusbaum <pn0534129@gmail.com>
2024-05-19 16:27:01 +0300

    May19

--- src/lending/Lending.sol
+++ src/lending/Lending.sol
@@ -12,11 +12,17 @@ contract Lending{
     address public owner;
 
     uint public rewardPerS;
-    uint public wad = 10 ** 18;
+    uint public totalRewards;
+    uint public collateralRatio = 10 * 1e18 / 10;
+    uint public percentFee = (3 * 9999997) * 1e18 / 1000;
 
     mapping (address => uint) public daiDeposits;
     mapping (address => uint) public depositsTime;
     mapping (address => uint) public rewards;
+    mapping (address => uint) public ethDeposits;
+    mapping (address => uint) public lendings;
+    mapping (address => uint) public lendingTime;
+    mapping (address => uint) public fees;
 
 
     constructor(address bond) {
@@ -33,6 +39,15 @@ contract Lending{
             depositsTime[user] = block.timestamp;
         }
         _;
+    }
+
+    modifier updateFee(address user){
+        if(lendings[user] > 0){
+            uint duration = block.timestamp - lendingTime[user];
+            fees[user] += (duration * (percentFee * lendings[msg.sender])) / 1e18;
+            lendingTime[user] = block.timestamp;
+        }
+        _; 
     }
 
     function depositDai(uint amount) external updateReward(msg.sender){
@@ -56,5 +71,29 @@ contract Lending{
         else{
             rewards[msg.sender] -= amount;
         }
+    }
+    receive() external payable{
+        ethDeposits[msg.sender] += msg.value;
+    }
+    
+    function lending(uint amount) external updateFee(msg.sender){
+        require(amount != 0, "amount equals 0");
+        require((amount * collateralRatio) / getPrice(0x2a1530C4C41db0B0b2bB646CB5Eb1A67b7158667) <= ethDeposits[msg.sender]);
+        require(amount <= daiToken.balanceOf(address(this)), "there isn't enough dai in the pool");
+        daiToken.transferFrom(address(this), msg.sender, amount);
+        lendings[msg.sender] += amount;
+    }
+
+    function loanRepayment(uint amount) external updateFee(msg.sender){
+        require(amount != 0, "amount equals 0");
+        require(amount == lendings[msg.sender] + fees[msg.sender], "your loan is greater");
+        totalRewards += fees[msg.sender];
+        fees[msg.sender] = 0;
+        lendings[msg.sender] = 0;
+        daiToken.transferFrom(msg.sender, address(this), amount);
+    }
+    function getPrice(address token) public returns (uint256 amount) {
+        require(token == address(daiToken) || token == 0x2a1530C4C41db0B0b2bB646CB5Eb1A67b7158667, "invalid-token"); // DAI or ETH
+        amount = token == address(daiToken) ? 3000 : 1;
     }
 }
